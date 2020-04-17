8fe0afc0ad7bc742e96c1006bf7e0a5c8a01739f
htmphuong <37945969+htmphuong@users.noreply.github.com>
2020-04-17 16:14:50 +0700

    Add files via upload

---
+++ NhanVien.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaper2OOP
+{
+    class NhanVien
+    {
+        private String ten;
+        private double hsl;
+        public NhanVien()
+        {
+
+        }
+        public NhanVien(String ten,double hsl)
+        {
+            this.ten = ten;
+            this.hsl = hsl;
+        }
+        public void NhapTT()
+        {
+            Console.WriteLine("Nhap ten =");
+            this.ten = Console.ReadLine();
+            Console.WriteLine("Nhap he so luong=");
+            this.hsl = Double.Parse(Console.ReadLine());
+        }
+        public double TinhLuong()
+        {
+            return (this.hsl * 1490000);
+        }
+        public void InTT()
+        {
+            Console.WriteLine("Ten nhan vien {0} co so luong la {1}", this.ten, this.TinhLuong());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaper2OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tao danh sach cac nhan vien
+            int n;
+            Console.WriteLine("Nhap so nhan vien");
+            n = Int32.Parse(Console.ReadLine());
+            NhanVien[] nv = new NhanVien[100];//tao mang nhan vien
+            for(int i = 0; i < n; i++)
+            {
+                nv[i] = new NhanVien();
+                nv[i].NhapTT();
+                nv[i].InTT();
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
